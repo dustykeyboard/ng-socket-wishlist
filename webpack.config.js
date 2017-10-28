e410392f19829c53d16607778c0b602ef2484310
Philip Boardman <dustykeyboard@brd.mn>
2017-10-28 19:41:23 +1100

    Init angular app

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack');
+module.exports = {
+  context: __dirname + '/app',
+  entry: {
+    app: './app.js',
+    vendor: ['angular']
+  },
+  output: {
+    path: __dirname + '/public/js',
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin(
+      /* chenkName= */ 'vendor',
+      /* filename = */ 'vendor.bundle.js'
+    )
+  ]
+};
