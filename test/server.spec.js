4af6b2ae155cf1bfb96c89270b970fe06f1e0e5c
Philip Boardman <dustykeyboard@brd.mn>
2017-10-28 23:45:12 +1100

    Creates and Broadcasts lists

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,4 +1,4 @@
-var should = require('should');
+var assert = require('assert');
 var io = require('socket.io-client');
 
 var server = require('../server');
@@ -29,10 +29,48 @@ describe('Chat Server', function() {
 
       client2.on('connect', () => {
         client2.on('connections', function(connections) {
-          connections.should.equal(2);
+          console.log('client2 connections', connections);
+          assert.equal(connections, 2);
           client1.disconnect();
           client2.disconnect();
           done();
+        });
+      });
+    });
+  });
+
+  it('Broadcasts lists', function(done) {
+    var client3 = io.connect(socketURL, options);
+    client3.on('connect', () => {
+      client3.on('lists', data => {
+        console.log('client3 received lists', data);
+        assert.deepEqual(data, []);
+        client3.disconnect();
+        done();
+      });
+    });
+  });
+
+  it('Creates and Broadcasts lists', function(done) {
+    var testListName = 'test list name';
+    var testList = { name: testListName };
+    var expectedResponse = [{ name: testListName, items: [] }];
+    var client4 = io.connect(socketURL, options);
+    client4.on('connect', () => {
+      client4.emit('createList', testList);
+      client4.on('lists', data => {
+        console.log('client4 received lists', data);
+        assert.deepEqual(data, []);
+        client4.disconnect();
+
+        var client5 = io.connect(socketURL, options);
+        client5.on('connect', () => {
+          client5.on('lists', data => {
+            console.log('client5 received lists', data);
+            assert.deepEqual(data, expectedResponse);
+            client5.disconnect();
+            done();
+          });
         });
       });
     });
