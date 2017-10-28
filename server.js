7088f9930613357827fc9c30821a4819dd7b0b81
Philip Boardman <dustykeyboard@brd.mn>
2017-10-28 19:22:28 +1100

    test server

--- server.js
+++ server.js
@@ -14,12 +14,17 @@ app.get('/', (req, res) => {
 
 io.on('connection', socket => {
   console.log(`connected (${socket.id})`);
+
   clients[socket.id] = {
     socket
   };
 
+  console.log('connections', Object.keys(clients).length);
+  io.sockets.emit('connections', Object.keys(clients).length);
+
   socket.on('disconnect', () => {
     console.log(`disconnected (${socket.id})`);
+    console.log('connections', Object.keys(clients).length);
     delete clients[socket.id];
   });
 });
